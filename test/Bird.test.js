503399b3f60eb6f439d64496f282a0f7647b7721
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-28 07:37:27 -0700

    bird model is good and valid

---
+++ test/Bird.test.js
@@ -0,0 +1,20 @@
+const { assert } = require('chai');
+const Bird = require('../lib/models/Bird');
+
+describe('Bird model', () => {
+    
+    it('is a good, valid model', () => {
+        let info = {
+            commonName: 'Anna\'s hummingbird',
+            scientificName: 'Calypte anna',
+            wingspan: '12 cm',
+            diet: 'nectar',
+            colors: ['magenta', 'gray', 'red']
+        };
+        const hummingbird = new Bird(info);
+        info._id = hummingbird._id;
+        assert.deepEqual(hummingbird.toJSON(), info);
+        assert.isUndefined(hummingbird.validateSync());
+    });
+
+});
